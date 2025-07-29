b976a0ef44a16a572bd93327de39ad15c492ac2b
Rachitheviking <53881531+public36@users.noreply.github.com>
2025-07-29 20:07:11 +0200

    Delete login.py

--- login.py
+++
@@ -1,6 +0,0 @@
-from telethon.sync import TelegramClient from telethon.sessions import StringSession
- 
-api_id = 7081826639  # ← دخّل API ID ديالك هنا api_hash = 'abcd1234abcd1234abcd1234abcd1234'  # ← دخّل API HASH ديالك هنا
- 
-with TelegramClient(StringSession(), api_id, api_hash) as client: print("Login successful") print("Here is your session string:") print(client.session.save())
- 
